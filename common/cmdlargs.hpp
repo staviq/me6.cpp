4f889cdb964847b5d76293450fc1f070a9786602
staviq <staviq@gmail.com>
2023-11-08 17:10:29 +0000

    c

--- common/cmdlargs.hpp
+++ common/cmdlargs.hpp
@@ -1,5 +1,5 @@
-#ifndef ME6_CPP_CMDLARGS_HPP
-#define ME6_CPP_CMDLARGS_HPP
+#ifndef ME6_CMDLARGS_HPP
+#define ME6_CMDLARGS_HPP
 
 #include "libs.hpp"
 
@@ -19,19 +19,19 @@ using json = nlohmann::json;
 class CmdlArgs
 {
     public:
-        CmdlArgs( int argc, char** argv )
-            : _self( argc ? argv[0] : "" ),
-              _args( argv + ( argc ? 1 : 0 ), argv + argc )
+        CmdlArgs ( int argc, char** argv )
+            : _self ( argc ? argv[0] : "" ),
+              _args ( argv + ( argc ? 1 : 0 ), argv + argc )
         {
         }
         ~CmdlArgs() {}
 
     private:
-        CmdlArgs( CmdlArgs& other )       = delete;
-        void operator=( const CmdlArgs& ) = delete;
+        CmdlArgs ( CmdlArgs& other )       = delete;
+        void operator= ( const CmdlArgs& ) = delete;
 
     public:
-        using ParseCallback = std::function< void( json& params ) >;
+        using ParseCallback = std::function< void ( json& params ) >;
 
         enum class ReqDef
         {
@@ -54,10 +54,10 @@ class CmdlArgs
         struct ValDef
         {
                 ValDef() {}
-                ValDef( ReqDef required, TypDef type = TypDef::Any, std::string placeholder = "" )
-                    : required( required ),
-                      type( type ),
-                      placeholder( placeholder )
+                ValDef ( ReqDef required, TypDef type = TypDef::Any, std::string placeholder = "" )
+                    : required ( required ),
+                      type ( type ),
+                      placeholder ( placeholder )
                 {
                 }
                 ReqDef      required;
@@ -67,21 +67,21 @@ class CmdlArgs
         struct ArgDef
         {
                 ArgDef() {}
-                ArgDef(
+                ArgDef (
                     std::string name,
                     std::set< std::string >
                                   aliases,
                     std::string   help,
                     ReqDef        required = ReqDef::Never,
-                    ValDef        value    = ValDef( ReqDef::Never ),
-                    ParseCallback callback = []( json& p ) { static_cast< void >( p ); }
+                    ValDef        value    = ValDef ( ReqDef::Never ),
+                    ParseCallback callback = [] ( json& p ) { static_cast< void > ( p ); }
                 )
-                    : name( name ),
-                      aliases( aliases ),
-                      help( help ),
-                      required( required ),
-                      value( value ),
-                      callback( callback )
+                    : name ( name ),
+                      aliases ( aliases ),
+                      help ( help ),
+                      required ( required ),
+                      value ( value ),
+                      callback ( callback )
                 {
                 }
                 std::string             name;
@@ -91,84 +91,84 @@ class CmdlArgs
                 ValDef                  value;
                 ParseCallback           callback;
 
-                bool   operator==( const ArgDef& t ) const { return ( this->aliases == t.aliases ); }
-                size_t operator()( const ArgDef& p ) const
+                bool   operator== ( const ArgDef& t ) const { return ( this->aliases == t.aliases ); }
+                size_t operator() ( const ArgDef& p ) const
                 {
                     size_t h = 0;
-                    for( const auto& alias : p.aliases )
+                    for ( const auto& alias : p.aliases )
                     {
-                        h ^= std::hash< std::string >()( alias );
+                        h ^= std::hash< std::string >() ( alias );
                     }
                     return h;
                 }
         };
 
     public:
-        void arg( ArgDef def )
+        void arg ( ArgDef def )
         {
-            const auto& newdef = *( _argdefs.insert( def ).first );
+            const auto& newdef = *( _argdefs.insert ( def ).first );
 
-            for( const auto& alias : newdef.aliases )
+            for ( const auto& alias : newdef.aliases )
             {
-                _aliases.emplace( alias, newdef );
+                _aliases.emplace ( alias, newdef );
             }
         }
 
         json parse()
         {
-            for( const auto& def : _argdefs )
+            for ( const auto& def : _argdefs )
             {
-                if( def.required == ReqDef::Required )
+                if ( def.required == ReqDef::Required )
                 {
                     bool found{ false };
-                    for( const auto& alias : def.aliases )
+                    for ( const auto& alias : def.aliases )
                     {
-                        if( std::find( _args.begin(), _args.end(), alias ) != _args.end() )
+                        if ( std::find ( _args.begin(), _args.end(), alias ) != _args.end() )
                         {
                             found = true;
                             break;
                         }
                     }
-                    if( !found )
+                    if ( !found )
                     {
                         std::stringstream e;
-                        e << "parameter " << json( def.aliases ).dump() << " is required:" << std::endl << "Parameter description:" << std::endl << def.help;
-                        throw std::invalid_argument( e.str() );
+                        e << "parameter " << json ( def.aliases ).dump() << " is required:" << std::endl << "Parameter description:" << std::endl << def.help;
+                        throw std::invalid_argument ( e.str() );
                     }
                 }
             }
             json j;
             j["self"] = _self;
-            for( auto arg = _args.begin(); arg != _args.end(); std::advance( arg, 1 ) )
+            for ( auto arg = _args.begin(); arg != _args.end(); std::advance ( arg, 1 ) )
             {
-                auto alias = _aliases.find( ( *arg ) );
-                if( alias == _aliases.end() )
+                auto alias = _aliases.find ( ( *arg ) );
+                if ( alias == _aliases.end() )
                 {
                     std::stringstream e;
                     e << "unrecognised parameter '" << *arg << "'";
-                    throw std::invalid_argument( e.str() );
+                    throw std::invalid_argument ( e.str() );
                 }
                 const auto& name = ( *alias ).first;
-                if( alias != _aliases.end() )
+                if ( alias != _aliases.end() )
                 {
                     const auto& def         = alias->second;
                     const auto& needs_value = def.value.required;
-                    if( needs_value == ReqDef::Never )
+                    if ( needs_value == ReqDef::Never )
                     {
                         j[def.name] = true;
 
                         // This one requires no value
                         continue;
                     }
-                    if( needs_value == ReqDef::Optional || needs_value == ReqDef::Required )
+                    if ( needs_value == ReqDef::Optional || needs_value == ReqDef::Required )
                     {
-                        if( arg == _args.end() || std::next( arg, 1 ) == _args.end() || _aliases.find( *std::next( arg, 1 ) ) != _aliases.end() )
+                        if ( arg == _args.end() || std::next ( arg, 1 ) == _args.end() || _aliases.find ( *std::next ( arg, 1 ) ) != _aliases.end() )
                         {
-                            if( needs_value == ReqDef::Required )
+                            if ( needs_value == ReqDef::Required )
                             {
                                 std::stringstream e;
                                 e << "parameter '" << name << "' expects an argument: " << std::endl << "Parameter description:" << std::endl << def.help;
-                                throw std::invalid_argument( e.str() );
+                                throw std::invalid_argument ( e.str() );
                             }
 
                             j[def.name] = true;
@@ -177,34 +177,34 @@ class CmdlArgs
                             continue;
                         }
 
-                        switch( def.value.type )
+                        switch ( def.value.type )
                         {
                         case TypDef::Any:
                         case TypDef::Text:
                         {
-                            j[def.name] = *std::next( arg, 1 );
+                            j[def.name] = *std::next ( arg, 1 );
                             break;
                         }
                         case TypDef::Float:
                         {
                             try
                             {
-                                auto tmp    = std::stold( *std::next( arg, 1 ) );
+                                auto tmp    = std::stold ( *std::next ( arg, 1 ) );
                                 j[def.name] = tmp;
                             }
-                            catch( std::exception& ce )
+                            catch ( std::exception& ce )
                             {
                                 std::stringstream e;
-                                e << "parameter '" << name << "' expects FLOAT argument, got '" << *std::next( arg, 1 ) << "': " << std::endl
+                                e << "parameter '" << name << "' expects FLOAT argument, got '" << *std::next ( arg, 1 ) << "': " << std::endl
                                   << "Parameter description:" << std::endl
                                   << def.help;
-                                throw std::invalid_argument( e.str() );
+                                throw std::invalid_argument ( e.str() );
                             }
                             break;
                         }
                         case TypDef::Integer:
                         {
-                            auto tmp    = std::stoll( *std::next( arg, 1 ) );
+                            auto tmp    = std::stoll ( *std::next ( arg, 1 ) );
                             j[def.name] = tmp;
                             break;
                         }
@@ -212,26 +212,26 @@ class CmdlArgs
                         {
                             try
                             {
-                                auto tmp    = std::stoull( *std::next( arg, 1 ) );
+                                auto tmp    = std::stoull ( *std::next ( arg, 1 ) );
                                 j[def.name] = tmp;
                             }
-                            catch( std::exception& ce )
+                            catch ( std::exception& ce )
                             {
                                 std::stringstream e;
-                                e << "parameter '" << name << "' expects UNSIGNED_INTEGER argument, got '" << *std::next( arg, 1 ) << "': " << std::endl
+                                e << "parameter '" << name << "' expects UNSIGNED_INTEGER argument, got '" << *std::next ( arg, 1 ) << "': " << std::endl
                                   << "Parameter description:" << std::endl
                                   << def.help;
-                                throw std::invalid_argument( e.str() );
+                                throw std::invalid_argument ( e.str() );
                             }
                             break;
                         }
                         default:
                         {
-                            j[def.name] = *std::next( arg, 1 );
+                            j[def.name] = *std::next ( arg, 1 );
                             break;
                         }
                         }
-                        std::advance( arg, 1 );
+                        std::advance ( arg, 1 );
 
                         // This one either has optional value, or required value was matched.
                         continue;
@@ -240,9 +240,9 @@ class CmdlArgs
             }
             _parsed = j;
 
-            for( const auto& def : _argdefs )
+            for ( const auto& def : _argdefs )
             {
-                def.callback( _parsed );
+                def.callback ( _parsed );
             }
 
             return _parsed;
